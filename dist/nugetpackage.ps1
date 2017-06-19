517a5d693c9a0e1cc8dd5e5858aab4083582f756
Marcel Singer <marcel.singer@live.de>
2017-06-19 20:53:25 +0200

    Modified dist directory

--- dist/nugetpackage.ps1
+++
@@ -1,8 +0,0 @@
-rm ./*.nupkg
-$apiKey = Get-Content "~\.nuget_api_key.txt"
-$src = "https://www.nuget.org/api/v2/package"
-nuget pack ../RandomCodeOrg.ENetFramework/RandomCodeOrg.ENetFramework.csproj
-nuget pack ../RandomCodeOrg.Pluto/RandomCodeOrg.Pluto.csproj
-
-Get-ChildItem *.nupkg* -recurse | forEach{ nuget push $_.Name $apiKey -Source $src }
-
